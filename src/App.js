bc49e99a86c47c69f3e4d1ab4b8cc648287553a9
larryh88 <103942231+larho888@users.noreply.github.com>
2022-08-16 20:55:15 -0400

    all logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    img: "",
+  });
+
+  const [num, setNum] = useState(123);
+
+  const [userChoice, setUserChoice] = useState('');
+
+  const [counter, setCounter] = useState(0);
+
+  const randomNumber = (num) => {
+    return Math.floor(Math.random() * num);
+  }
+
+  useEffect (() => {
+    setNum(randomNumber(252));
+  }, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(userChoice)
+    setNum(randomNumber(252));
+    setUserChoice('');
+    if (pokemon.name === userChoice) {
+      setCounter(prevCount => prevCount + 1);
+    }
+  };
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${num}`;
+
+useEffect(() => {
+  axios({
+    url: `${url}`,
+    method: "GET",
+    dataResponse: "json",
+    params: {
+      format: "json",
+    },
+  }).then((response) => {
+    setPokemon({ 
+      name: response.data.name, 
+      img: response.data.sprites.front_default })
+});
+}, [num]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="pokemonImg">
+        <h1>{pokemon.name}</h1>
+        <img src={pokemon.img} alt="" />
+        <form>
+          <input type="text" onChange={(e) => {
+            setUserChoice(e.target.value.toLowerCase());
+          }} value={userChoice} />
+          <button onClick={handleClick} >Guess!</button>
+        </form>
+        <div>
+          <p>Total Score: {counter}</p>
+        </div>
+      </div>
+
     </div>
   );
 }
